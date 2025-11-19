276399549baaf88a3320df2d5474af763d4739e0
Basavanagouda-017 <basavanagoudapatil017@gmail.com>
2025-11-19 14:33:08 +0530

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Level 3 React App Working</h1>
+      <p>This is a test React app for CI/CD.</p>
+    </div>
+  )
+}
